626200d42f8c2fdbbda2c685e3f989dfda3f3a45
danielorrala555-spec <danielorrala555@gmail.com>
2026-04-09 21:26:53 -0500

    define cat and food positions

--- cazando.js
+++ cazando.js
@@ -2,6 +2,14 @@ let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
  
 function graficarGato(){
-    ctx.fillStyle = "#F2EEED"
-    ctx.fillRect(canvas.width/4,canvas.height/4,canvas.width*2/4,canvas.height*2/4);
+    ctx.fillStyle = "#0b49e8"
+    ctx.fillRect(canvas.width/4,canvas.height/4,canvas.width*2/4+50,canvas.height/4);
+}
+function graficarComida(){
+    ctx.fillStyle = "#00ff95ea"
+    ctx.fillRect(0,0,100,100);
+}
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
 }
