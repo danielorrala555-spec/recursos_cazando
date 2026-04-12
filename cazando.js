f71edd75d25801db803d45c122d9cfe03105ae53
danielorrala555-spec <danielorrala555@gmail.com>
2026-04-12 18:21:21 -0500

    Add functions to move up, down, right and reduce the size of the cat

--- cazando.js
+++ cazando.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
 let gatoX = canvas.width / 4; 
+let gatoY = canvas.height / 4;
 
 function limpiarCanva() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -9,7 +10,7 @@ function limpiarCanva() {
 
 function graficarGato() {
     ctx.fillStyle = "#0b49e8";
-    ctx.fillRect(gatoX, canvas.height / 4, canvas.width * 2 / 4 + 50, canvas.height / 4);
+    ctx.fillRect(gatoX, gatoY, canvas.height / 4, canvas.width / 8 + 50, canvas.height / 4);
 }
 
 function graficarComida() {
@@ -25,7 +26,32 @@ function moverIzquierda() {
     graficarGato();    
 }
 
+function moverDerecha() {
+    gatoX += 10;
+
+    limpiarCanva();     
+    graficarComida();  
+    graficarGato();    
+}
+
+function moverArriba() {
+    gatoY -= 10;
+
+    limpiarCanva();     
+    graficarComida();  
+    graficarGato();    
+}
+
+function moverAbajo() {
+    gatoY += 10;
+
+    limpiarCanva();     
+    graficarComida();  
+    graficarGato();    
+}
+
+
 function iniciarJuego() {
     graficarGato();
     graficarComida();
-};
+}
