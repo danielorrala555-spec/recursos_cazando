4c50df4b67fbae4cfd5911137842004e6e842ec7
danielorrala555-spec <danielorrala555@gmail.com>
2026-04-12 01:10:14 -0500

    crear funcion limpiar canva y mover izquierda que permite mover al gato 10 pixeles a la izquierda

--- cazando.js
+++ cazando.js
@@ -1,15 +1,31 @@
-let canvas=document.getElementById("areaJuego");
-let ctx=canvas.getContext("2d");
- 
-function graficarGato(){
-    ctx.fillStyle = "#0b49e8"
-    ctx.fillRect(canvas.width/4,canvas.height/4,canvas.width*2/4+50,canvas.height/4);
+let canvas = document.getElementById("areaJuego");
+let ctx = canvas.getContext("2d");
+
+let gatoX = canvas.width / 4; 
+
+function limpiarCanva() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-function graficarComida(){
-    ctx.fillStyle = "#00ff95ea"
-    ctx.fillRect(0,0,100,100);
+
+function graficarGato() {
+    ctx.fillStyle = "#0b49e8";
+    ctx.fillRect(gatoX, canvas.height / 4, canvas.width * 2 / 4 + 50, canvas.height / 4);
 }
-function iniciarJuego(){
+
+function graficarComida() {
+    ctx.fillStyle = "#00ff95ea";
+    ctx.fillRect(0, 0, 100, 100);
+}
+
+function moverIzquierda() {
+    gatoX -= 10;
+
+    limpiarCanva();     
+    graficarComida();  
+    graficarGato();    
+}
+
+function iniciarJuego() {
     graficarGato();
     graficarComida();
-}
+};
